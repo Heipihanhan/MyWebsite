42382a5dd79ae04bf18069ce14ea04e36db9224d
Heipihanhan <heipihanhan@gmail.com>
2017-03-03 00:14:39 -0500

    8

--- js/sketch_colombian_conflict.js
+++ js/sketch_colombian_conflict.js
@@ -3,7 +3,7 @@ var maxLength = 750;
 var maxValue = 0;
 
 function preload(){
-	refugeeTable = loadTable('../data/03_Assignment_3_Data', 'xlsx', 'header');
+	victimTable = loadTable('../data/03_Assignment_3_Data', 'xlsx', 'header');
 	
 }
 
@@ -11,10 +11,10 @@ function preload(){
 function setup(){
   createCanvas(1000, 2000);
   textAlign(RIGHT, TOP);
-  print(refugeeTable.getRowCount());
-  print(refugeeTable.getColumnCount());
-  for (var i = 0; i < refugeeTable.getRowCount(); i++){
-  	maxValue = max(maxValue, refugeeTable.getNum(i, 'Total'));
+  print(victimTable.getRowCount());
+  print(victimTable.getColumnCount());
+  for (var i = 0; i < victimTable.getRowCount(); i++){
+  	maxValue = max(maxValue, victimTable.getNum(i, 'Total'));
   }
   print(maxValue);
 
@@ -23,8 +23,8 @@ function setup(){
 // ***** Draw function ***** //
 function draw(){
 	background(255);
-	for (var i = 0; i < refugeeTable.getRowCount(); i++){
-		var rectLength = map(refugeeTable.getNum(i, 'Total'), 0, maxValue, 0, maxLength); //map(value, min, max, min, max) convert scale, super useful 
+	for (var i = 0; i < victimTable.getRowCount(); i++){
+		var rectLength = map(victimTable.getNum(i, 'Total'), 0, maxValue, 0, maxLength); //map(value, min, max, min, max) convert scale, super useful 
 		rect(100, 50 + 20 * i, rectLength, 15);
 	}
 }

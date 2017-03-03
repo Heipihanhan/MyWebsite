62cf6666a023e134d1cce8035448c00b6838f57a
Heipihanhan <heipihanhan@gmail.com>
2017-03-02 23:42:01 -0500

    4

---
+++ js/sketch_columbian_conflict.js
@@ -0,0 +1,30 @@
+var refugeeTable; 
+var maxLength = 750; 
+var maxValue = 0;
+
+function preload(){
+	refugeeTable = loadTable('../data/RefugeesUNHCR.csv', 'csv', 'header');
+	
+}
+
+// ***** Setup function ***** //
+function setup(){
+  createCanvas(1000, 2000);
+  textAlign(RIGHT, TOP);
+  print(refugeeTable.getRowCount());
+  print(refugeeTable.getColumnCount());
+  for (var i = 0; i < refugeeTable.getRowCount(); i++){
+  	maxValue = max(maxValue, refugeeTable.getNum(i, 'Total'));
+  }
+  print(maxValue);
+
+}
+
+// ***** Draw function ***** //
+function draw(){
+	background(255);
+	for (var i = 0; i < refugeeTable.getRowCount(); i++){
+		var rectLength = map(refugeeTable.getNum(i, 'Total'), 0, maxValue, 0, maxLength); //map(value, min, max, min, max) convert scale, super useful 
+		rect(100, 50 + 20 * i, rectLength, 15);
+	}
+}

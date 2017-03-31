566af31fca3dd7a7b218934c4a87f115b8c95a11
Heipihanhan <heipihanhan@gmail.com>
2017-03-31 01:03:24 -0400

    fix

--- js/coding_exercise_2.js
+++ js/coding_exercise_2.js
@@ -1,4 +1,4 @@
-// ***** Global Variables ***** //
+// ***** Global variables ***** //
 var distance;
 
 // ***** Setup function ***** //
@@ -8,17 +8,18 @@ function setup(){
     distance = dist(0, 0, width*1.5, height*1.5);
 }
 
+// ***** Draw function ***** //
 function draw(){
-   background(255); 
-   noStroke();
-     for (var x = 12.5; x <= width; x = x + 25){
-         for (var y = 12.5; y <= height; y = y + 25){
-         var size = dist(mouseX, mouseY, x, y);
-         var s = size/distance*50;
-         var b = map(size, 0, 500, 0, 225);
-         fill(b);
-         ellipse(x, y, s, s);
-         }
-     } 
+    background(255); 
+    noStroke();
+    for (var x = 10; x <= width; x = x + 20){
+        for (var y = 10; y <= height; y = y + 20){
+            var size = dist(mouseX, mouseY, x, y);
+            var s = size/distance*50;
+            var b = map(size, 0, 500, 0, 225);
+            fill(b);
+            ellipse(x, y, s, s);
+        }
+    } 
 }
 

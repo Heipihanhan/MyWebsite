386d1009ad96e28193a97f88decfc3579b428eb9
Heipihanhan <heipihanhan@gmail.com>
2017-03-31 00:46:19 -0400

    fix size

--- js/coding_exercise_1.js
+++ js/coding_exercise_1.js
@@ -1,14 +1,14 @@
 function setup(){
-    createCanvas(800, 800);
+    createCanvas(400, 400);
 }
 
 function draw(){
     colorMode(HSB, width);
     noStroke();
-    for (var x = 0; x <= width; x = x + 20){
-        for (var y = 0; y <= height; y = y + 20){
+    for (var x = 0; x <= width; x = x + 10){
+        for (var y = 0; y <= height; y = y + 10){
             fill(x, y, width);
-            rect(x, y, 20, 20);
+            rect(x, y, 10, 10);
         }
     }
 }
